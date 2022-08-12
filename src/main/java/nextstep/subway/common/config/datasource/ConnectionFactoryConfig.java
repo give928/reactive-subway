89221ea3d95a20e68ed4b095ef33ed7ef77fa379
joohokim <give928@gmail.com>
2022-08-12 17:06:50 +0900

    refactor: Remove routing connection factory code

--- src/main/java/nextstep/subway/common/config/datasource/ConnectionFactoryConfig.java
+++ src/main/java/nextstep/subway/common/config/datasource/ConnectionFactoryConfig.java
@@ -9,9 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
-import org.springframework.r2dbc.connection.R2dbcTransactionManager;
-import org.springframework.transaction.ReactiveTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.time.Duration;
 
@@ -19,34 +16,17 @@ import java.time.Duration;
 @Configuration
 @ConfigurationPropertiesScan
 @EnableConfigurationProperties
-@EnableTransactionManagement
 @RequiredArgsConstructor
 public class ConnectionFactoryConfig extends AbstractR2dbcConfiguration {
     private final MasterConnectionProperties masterConnectionProperties;
-    private final SlaveConnectionProperties slaveConnectionProperties;
 
     @Bean
     @Override
     public ConnectionFactory connectionFactory() {
-        return new DynamicRoutingConnectionFactory(masterConnectionFactory(), slaveConnectionFactory());
-    }
-
-    private ConnectionFactory masterConnectionFactory() {
         return masterConnectionProperties.createConnectionPool();
     }
 
-    private ConnectionFactory slaveConnectionFactory() {
-        return slaveConnectionProperties.createConnectionPool();
-    }
-
-    @Bean
-    public ReactiveTransactionManager transactionManager(ConnectionFactory connectionFactory) {
-        return new R2dbcTransactionManager(connectionFactory);
-//        return new R2dbcTransactionManager(new DynamicRoutingConnectionFactory(masterConnectionFactory(), slaveConnectionFactory()));
-//        return new R2dbcTransactionManager(new TransactionAwareConnectionFactoryProxy(connectionFactory));
-    }
-
-    @ConfigurationProperties(prefix = "spring.r2dbc.master")
+    @ConfigurationProperties(prefix = "spring.r2dbc")
     public static class MasterConnectionProperties extends ConnectionProperties {
         public MasterConnectionProperties(String driver, String protocol, String host, Integer port, String username,
                                           String password, String database, Duration connectionTimeout, Boolean ssl,
@@ -54,20 +34,6 @@ public class ConnectionFactoryConfig extends AbstractR2dbcConfiguration {
                                           Duration maxLifeTime, Duration maxCreateConnectionTime,
                                           Duration maxAcquireTime, Integer acquireRetry, String validationQuery,
                                           Boolean registerJmx) {
-            super(driver, protocol, host, port, username, password, database, connectionTimeout, ssl, poolName,
-                  initialSize, maxSize, maxIdleTime, maxLifeTime, maxCreateConnectionTime, maxAcquireTime, acquireRetry,
-                  validationQuery, registerJmx);
-        }
-    }
-
-    @ConfigurationProperties(prefix = "spring.r2dbc.slave")
-    public static class SlaveConnectionProperties extends ConnectionProperties {
-        public SlaveConnectionProperties(String driver, String protocol, String host, Integer port, String username,
-                                         String password, String database, Duration connectionTimeout, Boolean ssl,
-                                         String poolName, Integer initialSize, Integer maxSize, Duration maxIdleTime,
-                                         Duration maxLifeTime, Duration maxCreateConnectionTime,
-                                         Duration maxAcquireTime, Integer acquireRetry, String validationQuery,
-                                         Boolean registerJmx) {
             super(driver, protocol, host, port, username, password, database, connectionTimeout, ssl, poolName,
                   initialSize, maxSize, maxIdleTime, maxLifeTime, maxCreateConnectionTime, maxAcquireTime, acquireRetry,
                   validationQuery, registerJmx);
